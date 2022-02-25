d522cbb8a06b6da40745e77937d283358560d0a5
Jonathan Souza <souzajonathan570@gmail.com>
2022-02-25 15:36:06 -0300

    adicionando configuração do Swagger

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { sales, products } = require('./routers');
+const swaggerUi = require('swagger-ui-express');
+const { sales, products } = require('./src/routers');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
